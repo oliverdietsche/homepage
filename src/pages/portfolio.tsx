5c7ca0ba28e6895113ed6b467370d95addace100
oliverdietsche <oliver.dietsche@gmail.com>
2023-06-21 22:12:50 +0200

    Remove email from portfolio

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -48,7 +48,7 @@ export default function PortfolioPage() {
 					text="Hi, I’m Oliver Dietsche and I’d call myself an enthusiastic software developer. This webpage acts as my digital portfolio and is entirely created by myself. You’ll get to know more about me as a person, my skill set, projects and more."
 				/>
 			</Section>
-			<Section palette={secondaryPalette}>
+			{/* <Section palette={secondaryPalette}>
 				<TextBlock
 					palette={primaryPalette}
 					title="– Contact"
@@ -64,8 +64,8 @@ export default function PortfolioPage() {
 						href="mailto:oliver@dietsche.email"
 					/>
 				</div>
-			</Section>
-			<Section palette={primaryPalette}>
+			</Section> */}
+			<Section palette={secondaryPalette}>
 				<div
 					css={css`
 						display: flex;
@@ -91,7 +91,7 @@ export default function PortfolioPage() {
 							grid-column: 2;
 						}
 					`}
-					palette={primaryPalette}
+					palette={secondaryPalette}
 					title="– Skills"
 					heading="Brief overview of the technologies I've worked with in the past."
 					text="I spent 4 years completing my apprenticeship in computer science computer during which I built a solid knowledge foundation of computers and learned a lot about software development.\n\n
@@ -99,7 +99,7 @@ export default function PortfolioPage() {
 					I have general knowledge in Java and experience using the Spring Framework to build a Web-App in combination with a SQL database and Thymeleaf to display the content. I'm also able to build websites using wordpress."
 				/>
 			</Section>
-			<Section palette={secondaryPalette}>
+			<Section palette={primaryPalette}>
 				<div
 					css={css`
 						display: grid;
@@ -113,7 +113,7 @@ export default function PortfolioPage() {
 								grid-column: span 2;
 							}
 						`}
-						palette={secondaryPalette}
+						palette={primaryPalette}
 						title="– Projects"
 						heading="Projects that I share on GitHub and other applications where I've been involved in development."
 						text="I organize most of my private coding projects publicly on GitHub so that any person interested in it can either use it, learn from it or participate and improve it."
@@ -121,14 +121,14 @@ export default function PortfolioPage() {
 					<Card
 						title="Autofiles"
 						description="This library supports automation of file creation in your project. It's highly configurable so that you can adjust it to your needs."
-						palette={primaryPalette}
+						palette={secondaryPalette}
 						icons={['typescript']}
 						githubHref="https://github.com/oliverdietsche/autofiles"
 					/>
 					<Card
 						title="Workshops - IPA"
 						description="I developed this application as part of my apprenticeship graduation. It's been developed for Namics, the company I've worked at during my apprenticeship and allows employees to plan and join workshops using their google accounts. Calendar events are created for every workshop."
-						palette={primaryPalette}
+						palette={secondaryPalette}
 						icons={['firebase', 'storybook', 'nextjs', 'typescript']}
 						githubHref="https://github.com/oliverdietsche/workshops-ipa"
 					/>
@@ -137,29 +137,29 @@ export default function PortfolioPage() {
 						description={
 							'An app to manage your tasks. The layout and functionality are inspired by a journal method called "Bullet Journal".'
 						}
-						palette={primaryPalette}
+						palette={secondaryPalette}
 						icons={['storybook', 'nextjs', 'typescript']}
 						githubHref="https://github.com/oliverdietsche/bullet-todo"
 					/>
 					<Card
 						title="WWDC 2019 Scholarships"
 						description="My submission to the 2019 WWDC scholarships which unfortunately got rejected. It's a binary game where you have to spin different layers of wheels with 0s and 1s on it to create binary codes matching the decimal on the outside. More information can be found in the GitHub repository."
-						palette={primaryPalette}
+						palette={secondaryPalette}
 						icons={['swift']}
 						githubHref="https://github.com/oliverdietsche/wwdc2019-scholarships"
 					/>
 					<Card
 						title="Homepage"
 						description="This page you're on right now."
-						palette={primaryPalette}
+						palette={secondaryPalette}
 						icons={['storybook', 'nextjs', 'typescript']}
 						githubHref="https://github.com/oliverdietsche/homepage"
 					/>
 				</div>
 			</Section>
-			<Section palette={primaryPalette}>
+			<Section palette={secondaryPalette}>
 				<TextBlock
-					palette={primaryPalette}
+					palette={secondaryPalette}
 					title="– Achievements"
 					heading="Achievements I’ve reached so far."
 				/>
@@ -168,23 +168,23 @@ export default function PortfolioPage() {
 						icon="plane"
 						text="3 weeks internship at Ocere in Cheltenham"
 						iconColor={universalColors.primary}
-						textColor={secondaryPalette.text.primary}
+						textColor={primaryPalette.text.primary}
 					/>
 					<IconFact
 						icon="trophy"
 						text="6th at the swiss Web Technologies championships"
 						iconColor={universalColors.primary}
-						textColor={secondaryPalette.text.primary}
+						textColor={primaryPalette.text.primary}
 					/>
 				</div>
 			</Section>
-			<Section palette={secondaryPalette}>
-				<TextBlock palette={secondaryPalette} title="– Testimonials" heading="What others say about my work." />
+			<Section palette={primaryPalette}>
+				<TextBlock palette={primaryPalette} title="– Testimonials" heading="What others say about my work." />
 				<Quote
 					author="Remi Ashton from Ocere"
 					quote='Over the three weeks, Oliver was part of our team we felt he grew into his role. He very quickly became one of the "leaders" of the group of lads he was working with.\n\nHe listened to instructions and worked very hard at executing what we had requested. He seemed to work very well in his team and was confident enough to come and ask myself or my team for help or confirmation on things he was not too sure with.\n\nAs the weeks went on Oliver seemed to grow in confidence and started interacting more and more with the team in the office.\n\nThe work he carried out was excellent, I think with more time and maybe a little more instruction from our side we would have the ideal CRM system, but what they have created is excellent.\n\nOliver was a pleasure to have in and around the office and would like to wish him the very best for his future. I’m sure whatever career path he decides to take he will excel in and will be a valid team member.'
-					palette={primaryPalette}
-					backgroundPalette={secondaryPalette}
+					palette={secondaryPalette}
+					backgroundPalette={primaryPalette}
 				/>
 			</Section>
 		</Fragment>
